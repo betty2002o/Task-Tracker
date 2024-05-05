00fc0e14cd7fb1844b63005ea73eb0c3e06f4948
Betty <betty2002o@gmail.com>
2024-05-04 21:28:45 -0700

    added body content layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import "./App.css";
 import TaskForm from "./components/TaskForm";
+import TaskColumn from "./components/TaskColumn";
+import todoIcon from "./assets/direct-hit.png";
+import doingIcon from "./assets/glowing-star.png";
+import doneIcon from "./assets/check-mark-button.png";
 
 function App() {
   return (
     <div className="app">
       <TaskForm />
       <main className="app_main">
-        <div className="toDo"> To Do</div>
-        <div className="doing"> Doing</div>
-        <div className="done"> Done</div>
+        <TaskColumn title="To Do" icon={todoIcon} />
+        <TaskColumn title="Doing" icon={doingIcon} />
+        <TaskColumn title="Done" icon={doneIcon} />
       </main>
     </div>
   );
---
+++ src/components/TaskColumn.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./TaskColumn.css";
+import TaskCard from "./TaskCard";
+
+const TaskColumn = ({ title, icon }) => {
+  return (
+    <section className="task_column">
+      <h2 className="task_column_heading">
+        <img className="task_column_icon" src={icon} alt="" /> {title}
+      </h2>
+      <TaskCard title="Change Delete Icon" tags={["HTML", "CSS"]} />
+      <TaskCard title="This is Sample Text" tags={["HTML", "CSS"]} />
+      <TaskCard title="This is Sample Text" tags={["HTML", "CSS"]} />
+      <TaskCard title="This is Sample Text" tags={["HTML", "CSS"]} />
+      <TaskCard title="This is Sample Text" tags={["HTML", "CSS"]} />
+    </section>
+  );
+};
+
+export default TaskColumn;
